8f2dfc404a965a060611e55122aefdb822e0a817
wubo <wubo@simulite.com>
2017-04-24 07:07:49 +0800

    as is

---
+++ webpack.config.prod.js
@@ -0,0 +1,50 @@
+import path from 'path';
+import webpack from "webpack"
+
+export default {
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+      noInfo: false}),
+    // Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    // Minify JS
+    new webpack.optimize.UglifyPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ],
+      }
+    ]
+  }
+};
